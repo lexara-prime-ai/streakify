7ddd9ed23e6eeaeae8c5d3e25ee061513b7a6d75
irfanghat <irfanghat@gmail.com>
2023-04-28 18:04:04 +0300

    add habit cards

--- build/App.js
+++ build/App.js
@@ -1,3 +1,24 @@
 "use strict";
 // DEBUGGING
 const log = console.log;
+class App {
+    // static render() {
+    // }
+    static dipslayHabit() {
+        const userInput = document.querySelector('#user-input');
+        const habit = document.querySelector('.habit');
+        habit.innerHTML = `Track your <span>${userInput.value}</span> habit..`;
+        log(userInput.value);
+    }
+    static displayStreakCount() {
+        let date = new Date();
+        let days = date.getMonth();
+        // DAY COUNT
+        const count = document.querySelector('.count');
+        count.innerHTML = days.toString();
+        log(days);
+    }
+}
+const addBtn = document.querySelector('#add-btn');
+addBtn.addEventListener('click', App.dipslayHabit);
+addBtn.addEventListener('click', App.displayStreakCount);
--- src/App.ts
+++ src/App.ts
@@ -2,22 +2,30 @@
 const log = console.log;
 
 class App {
-    static render() {
+    // static render() {
 
-    }
+    // }
 
     static dipslayHabit() {
         const userInput = document.querySelector('#user-input')! as HTMLInputElement;
-        const habit = document.querySelector('.habit');
-
-        log(habit);
+        const habit = document.querySelector('.habit') as HTMLElement;
+        habit.innerHTML = `Track your <span>${userInput.value}</span> habit..`;
+        log(userInput.value);
     }
 
     static displayStreakCount() {
+        let date = new Date();
+        let days = date.getMonth();
 
+        // DAY COUNT
+        const count = document.querySelector('.count') as HTMLElement;
+        count.innerHTML = days.toString();
+
+        log(days);
     }
 }
 
-const addBtn = document.querySelector('.add-btn') as HTMLElement;
+const addBtn = document.querySelector('#add-btn') as HTMLElement;
 
 addBtn.addEventListener('click', App.dipslayHabit);
+addBtn.addEventListener('click', App.displayStreakCount);
