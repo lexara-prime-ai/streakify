2c56eba1c2fc3f86e4309c094d3d9414ff71f161
irfanghat <irfanghat@gmail.com>
2023-04-28 21:33:11 +0300

    add habit card on click

--- build/App.js
+++ build/App.js
@@ -6,8 +6,8 @@ class App {
     // }
     static dipslayHabit() {
         const userInput = document.querySelector('#user-input');
-        const habit = document.querySelector('.habit');
-        habit.innerHTML = `Track your <span>${userInput.value}</span> habit..`;
+        const activeHabit = document.querySelector('.active-habit');
+        activeHabit.innerHTML = `Track your <span>${userInput.value}</span> habit..`;
         log(userInput.value);
     }
     static displayStreakCount() {
@@ -18,7 +18,30 @@ class App {
         count.innerHTML = days.toString();
         log(days);
     }
+    static addHabit() {
+        const userInput = document.querySelector('#user-input');
+        const habitContainer = document.querySelector('.habits');
+        habitContainer.innerHTML += `
+        <div class="habit-container">
+            <div class="habit">
+
+            <h3 class="habit-title">
+                ${userInput.value}
+            </h3>  
+
+                <i class="fas fa-clock clock-icon"></i>
+            </div>
+
+            <div class="date">
+                <h3 class="date-display">
+                    Date started
+                </h3>
+            </div>
+        </div>
+        `;
+    }
 }
 const addBtn = document.querySelector('#add-btn');
 addBtn.addEventListener('click', App.dipslayHabit);
 addBtn.addEventListener('click', App.displayStreakCount);
+addBtn.addEventListener('click', App.addHabit);
--- src/App.ts
+++ src/App.ts
@@ -8,8 +8,8 @@ class App {
 
     static dipslayHabit() {
         const userInput = document.querySelector('#user-input')! as HTMLInputElement;
-        const habit = document.querySelector('.habit') as HTMLElement;
-        habit.innerHTML = `Track your <span>${userInput.value}</span> habit..`;
+        const activeHabit = document.querySelector('.active-habit') as HTMLElement;
+        activeHabit.innerHTML = `Track your <span>${userInput.value}</span> habit..`;
         log(userInput.value);
     }
 
@@ -23,9 +23,36 @@ class App {
 
         log(days);
     }
+
+    static addHabit() {
+        const userInput = document.querySelector('#user-input')! as HTMLInputElement;
+        const habitContainer = document.querySelector('.habits') as HTMLElement;
+        habitContainer.innerHTML += `
+        <div class="habit-container">
+            <div class="habit">
+
+            <h3 class="habit-title">
+                ${userInput.value}
+            </h3>  
+
+                <i class="fas fa-clock clock-icon"></i>
+            </div>
+
+            <div class="date">
+                <h3 class="date-display">
+                    Date started
+                </h3>
+            </div>
+        </div>
+        `;
+    }
 }
 
 const addBtn = document.querySelector('#add-btn') as HTMLElement;
 
 addBtn.addEventListener('click', App.dipslayHabit);
 addBtn.addEventListener('click', App.displayStreakCount);
+addBtn.addEventListener('click', App.addHabit);
+
+
+
